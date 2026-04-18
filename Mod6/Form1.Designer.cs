b7e738d85c3b4e607eb0f609ab2c1dac8f0290e5
Limolix <andrejnagirnak395@gmail.com>
2026-04-18 10:33:22 +0800

    We control the emitter

--- Mod6/Form1.Designer.cs
+++ Mod6/Form1.Designer.cs
@@ -31,7 +31,10 @@
             components = new System.ComponentModel.Container();
             picDisplay = new PictureBox();
             timer1 = new System.Windows.Forms.Timer(components);
+            tbDirection = new TrackBar();
+            lblDirection = new TextBox();
             ((System.ComponentModel.ISupportInitialize)picDisplay).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)tbDirection).BeginInit();
             SuspendLayout();
             // 
             // picDisplay
@@ -49,21 +52,43 @@
             timer1.Interval = 40;
             timer1.Tick += timer1_Tick;
             // 
+            // tbDirection
+            // 
+            tbDirection.Location = new Point(12, 444);
+            tbDirection.Maximum = 359;
+            tbDirection.Name = "tbDirection";
+            tbDirection.Size = new Size(214, 45);
+            tbDirection.TabIndex = 1;
+            tbDirection.Scroll += tbDirection_Scroll;
+            // 
+            // lblDirection
+            // 
+            lblDirection.Location = new Point(232, 444);
+            lblDirection.Name = "lblDirection";
+            lblDirection.Size = new Size(29, 23);
+            lblDirection.TabIndex = 2;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 538);
+            Controls.Add(lblDirection);
+            Controls.Add(tbDirection);
             Controls.Add(picDisplay);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)picDisplay).EndInit();
+            ((System.ComponentModel.ISupportInitialize)tbDirection).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox picDisplay;
         private System.Windows.Forms.Timer timer1;
+        private TrackBar tbDirection;
+        private TextBox lblDirection;
     }
 }
--- Mod6/Form1.cs
+++ Mod6/Form1.cs
@@ -8,38 +8,59 @@ namespace Mod6
         private int MousePositionX = 0;
         private int MousePositionY = 0;
 
-        Emitter emitter;
+        List<Emitter> emitters = new List<Emitter>();
+        Emitter emitter; // добавим поле дл€ эмиттера
+
+
+
+
         public Form1()
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
 
 
-            emitter = new TopEmitter
-            {
-                Width = picDisplay.Width,
-                GravitationY = 0.25f
-            };
-            // гравитон
-            emitter.impactPoints.Add(new GravityPoint
-            {
-                X = (float)(picDisplay.Width * 0.25),
-                Y = picDisplay.Height / 2
-            });
+            //emitter = new TopEmitter
+            //{
+            //    Width = picDisplay.Width,
+            //    GravitationY = 0.25f
+            //};
+            //// гравитон
+            //emitter.impactPoints.Add(new GravityPoint
+            //{
+            //    X = (float)(picDisplay.Width * 0.25),
+            //    Y = picDisplay.Height / 2
+            //});
 
-            // в центре антигравитон
-            emitter.impactPoints.Add(new AntiGravityPoint
+            //// в центре антигравитон
+            //emitter.impactPoints.Add(new AntiGravityPoint
+            //{
+            //    X = picDisplay.Width / 2,
+            //    Y = picDisplay.Height / 2
+            //});
+
+            //// снова гравитон
+            //emitter.impactPoints.Add(new GravityPoint
+            //{
+            //    X = (float)(picDisplay.Width * 0.75),
+            //    Y = picDisplay.Height / 2
+            //});
+
+
+            this.emitter = new Emitter // создаю эмиттер и прив€зываю его к полю emitter
             {
+                Direction = 0,
+                Spreading = 10,
+                SpeedMin = 10,
+                SpeedMax = 10,
+                ColorFrom = Color.Gold,
+                ColorTo = Color.FromArgb(0, Color.Red),
+                ParticlesPerTick = 10,
                 X = picDisplay.Width / 2,
-                Y = picDisplay.Height / 2
-            });
+                Y = picDisplay.Height / 2,
+            };
 
-            // снова гравитон
-            emitter.impactPoints.Add(new GravityPoint
-            {
-                X = (float)(picDisplay.Width * 0.75),
-                Y = picDisplay.Height / 2
-            });
+            emitters.Add(this.emitter); // все равно добавл€ю в список emitters, чтобы он рендерилс€ и обновл€лс€
         }
 
 
@@ -51,7 +72,7 @@ namespace Mod6
 
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                
+
                 g.Clear(Color.Black);
                 emitter.Render(g); // рендерим систему
             }
@@ -63,6 +84,12 @@ namespace Mod6
         {
             emitter.MousePositionX = e.X;
             emitter.MousePositionY = e.Y;
+        }
+
+        private void tbDirection_Scroll(object sender, EventArgs e)
+        {
+            emitter.Direction = tbDirection.Value; // направлению эмиттера присваиваем значение ползунка 
+            lblDirection.Text = $"{tbDirection.Value}∞"; // добавил вывод значени€
         }
     }
 }
